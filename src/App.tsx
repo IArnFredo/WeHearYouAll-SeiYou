6749de1a4546ef19f39ec86cf66bcd267ac7712f
Julius Alfredo <ju.27.al@gmail.com>
2022-04-07 09:09:22 +0700

    Enter

--- src/App.tsx
+++ src/App.tsx
@@ -28,9 +28,9 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
+        {/* <Route exact path="/home">
           <Home />
-        </Route>
+        </Route> */}
         <Route exact path="/">
           <Redirect to="/home" />
         </Route>
