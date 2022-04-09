c676c949fe8abeb027ad0bbe802cbc4a4eac0434
MjsTy <yudhistira.wardhana@gmail.com>
2022-04-09 22:37:46 +0700

    barebone UI

--- src/pages/AnotherProfile.tsx
+++ src/pages/AnotherProfile.tsx
@@ -1,22 +1,31 @@
-import { IonButton, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonRow } from '@ionic/react'
+import { IonBackButton, IonButton, IonButtons, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react'
 import React from 'react';
 import './Profile.css';
 
 const AnotherProfile = () => {
   return (
-    <IonContent fullscreen className='ion-padding' id='bg'>
-            <IonRow>
-                <IonCol size-sm="8" offset-sm="2" size-md="6" offset-md="3">
-                    <img className='avatar-profile' src='./assets/images/shionne.jpg' />
-                        <IonCardHeader class='text-profile'>
-                            <IonCardTitle>Yudhistira Aremaputra Wardhana</IonCardTitle>
-                            
-                            <IonCardSubtitle>Male, 20</IonCardSubtitle>
-                            
-                        </IonCardHeader>
-                </IonCol>
-            </IonRow>     
-    </IonContent>
+    <IonPage>
+      <IonToolbar>
+        <IonButtons slot="start">
+          <IonBackButton defaultHref="/" />
+        </IonButtons>
+        <IonTitle>Back Button</IonTitle>
+      </IonToolbar>
+      <IonContent fullscreen className='ion-padding' id='bg'>
+        <IonRow>
+          <IonCol size-sm="8" offset-sm="2" size-md="6" offset-md="3">
+            <img className='avatar-profile' src='./assets/images/shionne.jpg' />
+            <IonCardHeader class='text-profile'>
+              <IonCardTitle>Yudhistira Aremaputra Wardhana</IonCardTitle>
+                        
+              <IonCardSubtitle>Male, 20</IonCardSubtitle>
+                        
+            </IonCardHeader>
+          </IonCol>
+        </IonRow>     
+      </IonContent>
+    </IonPage>
+    
   )
 }
 
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,28 +1,31 @@
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonIcon, IonRow } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonIcon, IonPage, IonRow } from '@ionic/react';
 import { arrowUpOutline, logoInstagram, pencilOutline, playOutline } from 'ionicons/icons';
 import React from 'react';
 import './Profile.css';
 
 const Profile = () => {
   return (
-    <IonContent fullscreen className='ion-padding' id='bg'>
+    <IonPage>
+        <IonContent fullscreen className='ion-padding' id='bg'>
             <IonRow>
                 <IonCol size-sm="8" offset-sm="2" size-md="6" offset-md="3">
                     <img className='avatar-profile' src='./assets/images/shionne.jpg' />
-                        <IonCardHeader class='text-profile'>
-                            <IonCardTitle>Yudhistira Aremaputra Wardhana</IonCardTitle>
+                    <IonCardHeader class='text-profile'>
+                        <IonCardTitle>Yudhistira Aremaputra Wardhana</IonCardTitle>
+                                
+                        <IonCardSubtitle>Male, 20</IonCardSubtitle>
                             
-                            <IonCardSubtitle>Male, 20</IonCardSubtitle>
-                            
-                        </IonCardHeader>
-                        <IonCardContent>
-                            <IonButton href='' expand="block"><IonIcon className='button-icon' icon={pencilOutline}/>Edit Profile</IonButton>
-                            <IonButton href='' expand="block"><IonIcon className='button-icon' icon={playOutline}/>Your Voices</IonButton>
-                            <IonButton href='' expand="block"><IonIcon className='button-icon' icon={arrowUpOutline}/>Upload New Voices</IonButton>
-                        </IonCardContent>
+                    </IonCardHeader>
+                    <IonCardContent>
+                        <IonButton href='' expand="block"><IonIcon className='button-icon' icon={pencilOutline}/>Edit Profile</IonButton>
+                        <IonButton href='' expand="block"><IonIcon className='button-icon' icon={playOutline}/>Your Voices</IonButton>
+                        <IonButton href='' expand="block"><IonIcon className='button-icon' icon={arrowUpOutline}/>Upload New Voices</IonButton>
+                    </IonCardContent>
                 </IonCol>
             </IonRow>     
-    </IonContent>
+        </IonContent>
+    </IonPage>
+    
   );
 };
 
