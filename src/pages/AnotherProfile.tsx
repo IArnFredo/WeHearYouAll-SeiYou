8b77e875515d2c92ef5d473151c53eb234f0c4ff
MjsTy <yudhistira.wardhana@gmail.com>
2022-04-09 22:16:54 +0700

    commit on 'Profiles'

--- src/pages/AnotherProfile.tsx
+++ src/pages/AnotherProfile.tsx
@@ -1,9 +1,22 @@
 import { IonButton, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonRow } from '@ionic/react'
-import React from 'react'
+import React from 'react';
+import './Profile.css';
 
 const AnotherProfile = () => {
   return (
-    <p>Another Profile</p>
+    <IonContent fullscreen className='ion-padding' id='bg'>
+            <IonRow>
+                <IonCol size-sm="8" offset-sm="2" size-md="6" offset-md="3">
+                    <img className='avatar-profile' src='./assets/images/shionne.jpg' />
+                        <IonCardHeader class='text-profile'>
+                            <IonCardTitle>Yudhistira Aremaputra Wardhana</IonCardTitle>
+                            
+                            <IonCardSubtitle>Male, 20</IonCardSubtitle>
+                            
+                        </IonCardHeader>
+                </IonCol>
+            </IonRow>     
+    </IonContent>
   )
 }
 
