2c7c4c6e1b49859b7f34939531c9eeeef572e86b
Alvin <alvinmartindwrk@gmail.com>
2022-04-09 21:54:34 +0700

    base structure for upload and edit voice

--- src/pages/EditVoice.tsx
+++ src/pages/EditVoice.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { IonBackButton, IonButton, IonButtons, IonCol, IonContent, IonGrid, IonInput, IonItem, IonLabel, IonPage, IonRow, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useRef } from 'react';
 
-const EditVoice = () => {
+const EditVoice: React.FC = () => {
+  const voiceNameRef = useRef<HTMLIonInputElement>(null);
+
   return (
-    <div>EditVoice</div>
-  )
-}
+    <IonPage>
+      <IonToolbar>
+        <IonButtons slot="start">
+          <IonBackButton defaultHref='/@your-voice-list' />
+        </IonButtons>
+        <IonTitle>Upload New Voice</IonTitle>
+      </IonToolbar>
+      <IonContent className='ion-padding'>
+        <IonGrid className='ion-text-center'>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonInput ref={voiceNameRef} placeholder="Voice Name"></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="stacked">Voice Description</IonLabel>
+                <IonTextarea placeholder="Voice description..."></IonTextarea>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonButton>Update Voice</IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
+};
 
-export default EditVoice
+export default EditVoice;
--- src/pages/UploadVoice.tsx
+++ src/pages/UploadVoice.tsx
@@ -1,16 +1,44 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonBackButton, IonButton, IonButtons, IonCol, IonContent, IonGrid, IonInput, IonItem, IonLabel, IonPage, IonRow, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useRef } from 'react';
 
 const UploadVoice: React.FC = () => {
+  const voiceNameRef = useRef<HTMLIonInputElement>(null);
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Upload New Voice</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      <IonToolbar>
+        <IonButtons slot="start">
+          <IonBackButton defaultHref='/@profile' />
+        </IonButtons>
+        <IonTitle>Upload New Voice</IonTitle>
+      </IonToolbar>
       <IonContent className='ion-padding'>
-        <h1>upload voice</h1>
+        <IonGrid className='ion-text-center'>
+          <IonRow>
+            <IonButton>Select Voice</IonButton>
+            <IonCol>No voice file selected</IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonInput ref={voiceNameRef} placeholder="Voice Name"></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="stacked">Voice Description</IonLabel>
+                <IonTextarea placeholder="Voice description..."></IonTextarea>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonButton>Upload Voice</IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
