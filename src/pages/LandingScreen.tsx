b59115c6326ec87d50a7b364c9523ac2f04e5d60
JasonJT01 <jason.tandiono@student.umn.ac.id>
2022-04-10 00:13:21 +0700

    Landing Screen & Record Voice

--- src/pages/LandingScreen.tsx
+++ src/pages/LandingScreen.tsx
@@ -1,11 +1,22 @@
-import { IonContent, IonHeader, IonPage, IonButton, IonTitle } from '@ionic/react';
+import { IonContent, IonPage, IonButton, IonGrid, IonRow, IonLabel, IonText } from '@ionic/react';
 import './Home.css';
 
 const LandingScreen: React.FC = () => {
   return (
     <IonPage>
       <IonContent className='content' fullscreen>
-
+        <div>
+          <IonButton className='skipButton' routerLink={'/'} fill='clear' color='light'>Skip</IonButton>
+          <img src="../assets/images/landing.png" alt="" className='landingImg' />
+        </div>
+        <IonGrid className='ion-text-center'>
+          <IonLabel className='text1'>Welcome to SeiYou</IonLabel><br />
+          <IonLabel className='text2'>Start your voice act here, it's free!</IonLabel>
+          <IonRow className='ion-margin-top ion-justify-content-center'>
+            <IonButton fill='clear' shape='round' color='dark' routerLink={'/@register'}>Sign Up</IonButton>
+            <IonButton shape='round' routerLink={'/@login'}>Sign In</IonButton>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
